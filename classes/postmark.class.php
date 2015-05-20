187e6bcf2ff5cc9787bca8c7c654d575052441b4
Kijin Sung <kijin@kijinsung.com>
2015-05-20 22:12:38 +0900

    Fix incorrect namespace reference in Postmark sender

--- classes/postmark.class.php
+++ classes/postmark.class.php
@@ -6,7 +6,7 @@ class Xternal_Mailer_Postmark extends Xternal_Mailer_Base
 	{
 		$this->procAssembleMessage();
 		
-		$transport = \Swift_PostmarkTransport::newInstance(self::$config->password);
+		$transport = \Openbuildings\Postmark\Swift_PostmarkTransport::newInstance(self::$config->password);
 		$mailer = \Swift_Mailer::newInstance($transport);
 		$result = $mailer->send($this->message, $this->errors);
 		return (bool)$result;
