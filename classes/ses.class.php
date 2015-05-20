dcd1a08e09d920d0e7d2a6b72974a8bc357a2de3
Kijin Sung <kijin@kijinsung.com>
2015-05-20 22:22:01 +0900

    Fix missing endpoint and debugging in AWS SES sender

--- classes/ses.class.php
+++ classes/ses.class.php
@@ -6,9 +6,41 @@ class Xternal_Mailer_Ses extends Xternal_Mailer_Base
 	{
 		$this->procAssembleMessage();
 		
+		$debug = array($this, 'debugCallback');
+		$endpoint = 'https://email.us-east-1.amazonaws.com/';
+		if(preg_match('/^(us|eu|ap)-(north|south)?(east|west)-[0-9a-z]+$/i', self::$config->smtp_host))
+		{
+			$endpoint = 'https://email.' . strtolower(self::$config->smtp_host) . '.amazonaws.com/';
+		}
+		
 		$transport = \Swift_AWSTransport::newInstance(self::$config->username, self::$config->password);
-		$mailer = \Swift_Mailer::newInstance($transport);
-		$result = $mailer->send($this->message, $this->errors);
-		return (bool)$result;
+		$transport->setDebug($debug);
+		$transport->setEndpoint($endpoint);
+		
+		try
+		{
+			$mailer = \Swift_Mailer::newInstance($transport);
+			$result = $mailer->send($this->message, $this->errors);
+		}
+		catch(\Exception $e)
+		{
+			$this->errors = array('Amazon SES: ' . $e->getMessage());
+			return false;
+		}
+		
+		if($result)
+		{
+			$this->errors = array();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public function debugCallback($msg)
+	{
+		$this->errors[] = $msg;
 	}
 }
