2ad8fda21e11de19ecb710007b0860180b841c24
Kijin Sung <kijin@kijinsung.com>
2015-06-02 17:14:06 +0900

    Add meaningful error messages to Woorimail driver

--- classes/woorimail.class.php
+++ classes/woorimail.class.php
@@ -4,6 +4,21 @@ namespace Advanced_Mailer;
 
 class Woorimail extends Base
 {
+	public static $_error_codes = array(
+		'me_001' => '@ 없는 이메일 주소가 있습니다.',
+		'me_002' => '이메일 주소가 존재하지 않습니다.',
+		'me_003' => '닉네임이 존재하지 않습니다.',
+		'me_004' => '등록일이 존재하지 않습니다.',
+		'me_005' => '이메일과 닉네임 갯수가 다릅니다.',
+		'me_006' => '닉네임과 등록일 갯수가 다릅니다.',
+		'me_007' => '이메일과 등록일 갯수가 다릅니다.',
+		'me_008' => '이메일 갯수가 2,000개 넘습니다.',
+		'me_009' => 'type이 api가 아닙니다.',
+		'me_010' => '인증키가 없습니다.',	
+		'me_011' => '인증키가 부정확합니다.',
+		'me_012' => '포인트가 부족합니다.',
+	);
+	
 	public function send()
 	{
 		$data = array(
@@ -73,6 +88,10 @@ class Woorimail extends Base
 		{
 			if(isset($result['error_msg']))
 			{
+				if(isset(self::$_error_codes[$result['error_msg']]))
+				{
+					$result['error_msg'] .= ' ' . self::$_error_codes[$result['error_msg']];
+				}
 				$this->errors = array('Woorimail: ' . $result['error_msg']);
 			}
 			return false;
