ad9e40833da888be32ec9c6602a41fe9084d4f28
Kijin Sung <kijin@kijinsung.com>
2015-06-09 12:58:21 +0900

    Simplify config loading in trigger

--- advanced_mailer.model.php
+++ advanced_mailer.model.php
@@ -6,10 +6,8 @@ class Advanced_MailerModel extends Advanced_Mailer
 	{
 		if(class_exists('Mail', false)) return;
 		
-		$config = $this->getConfig();
-		
 		include_once __DIR__ . '/classes/base.class.php';
 		class_alias('Advanced_Mailer\\Base', 'Mail');
-		Mail::$config = $config;
+		Mail::$config = $this->getConfig();
 	}
 }
