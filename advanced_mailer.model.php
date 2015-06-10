058e4f5f24f83299276c532c9b31f464d30bfffe
Kijin Sung <kijin@kijinsung.com>
2015-06-10 13:47:22 +0900

    Do not load if PHP version is lower than 5.4

--- advanced_mailer.model.php
+++ advanced_mailer.model.php
@@ -15,6 +15,7 @@ class Advanced_MailerModel extends Advanced_Mailer
 	public function triggerReplaceMailClass()
 	{
 		if(class_exists('Mail', false)) return;
+		if(version_compare(PHP_VERSION, '5.4', '<')) return;
 		
 		include_once __DIR__ . '/classes/base.class.php';
 		class_alias('Advanced_Mailer\\Base', 'Mail');
